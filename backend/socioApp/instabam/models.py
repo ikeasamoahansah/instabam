6fe9dec3dd76a1d5f1f81d4b10f4cf66ab63753d
Me45y63 <dylanraspino264@gmail.com>
2023-05-06 19:24:43 +0000

    first work on backend

--- backend/socioApp/instabam/models.py
+++ backend/socioApp/instabam/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption_text = models.CharField(blank=True, max_length=64)
+    body = models.ImageField(blank=False, upload_to="images/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author}, {self.created_at}"
