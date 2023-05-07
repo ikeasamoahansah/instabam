ac679595fa9d52eb1ed8bc8e4e78f22552fb3841
Me45y63 <dylanraspino264@gmail.com>
2023-05-07 21:41:21 +0000

    post view working!

--- backend/socioApp/instabam/views.py
+++ backend/socioApp/instabam/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from .models import *
 
 # Create your views here.
+def home(request):
+    return render(request, "instabam/home.html", {
+        "posts": Post.objects.all()
+    })
+
+def logout_view(request):
+    logout(request)
