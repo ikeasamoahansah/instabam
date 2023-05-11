8f94b4502e9ab3d2764fb2588808326627c80831
Ike <dylanraspino264@gmail.com>
2023-05-11 18:03:09 +0000

    added .env file

--- backend/socioApp/socioApp/settings.py
+++ backend/socioApp/socioApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'servertestpython87@gmail.com'
-EMAIL_HOST_PASSWORD = 'dvjwckauujxbtrcn'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
