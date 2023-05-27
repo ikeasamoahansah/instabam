a41582cb6a787fe37f79d9ce122df1c49f728aa1
Ike <dylanraspino264@gmail.com>
2023-05-27 20:25:49 +0000

    checkout

--- backend/socioApp/instabam/models.py
+++ backend/socioApp/instabam/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     caption_text = models.CharField(blank=True, max_length=64)
     body = models.ImageField(blank=False, upload_to="images/")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,3 +13,16 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.author}, {self.created_at}"
+
+
+class RePost(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} retweeted {self.tweet}'
+    
+    class Meta:
+        ordering = ['-created_at']
