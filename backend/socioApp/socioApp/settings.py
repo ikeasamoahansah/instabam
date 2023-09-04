639920f61c28e883a4ad8b00d1e821e234576d0d
Ike <dylanraspino264@gmail.com>
2023-09-04 17:05:32 +0000

    changed database to use local postgres database

--- backend/socioApp/socioApp/settings.py
+++ backend/socioApp/socioApp/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'socioApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
+        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
 
